6f35537e168e189d943614709670eadc33298e92
ivanov <ivanov@SSD1400>
2023-01-18 20:17:08 +0200

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int func1(int x)
+{
+	return x;
+}
+int func2(int x)
+{
+	return x;
+}
+
+int func3(int x)
+{
+	return x;
+}
+
+struct MyStruct
+{
+
+};
+int main()
+{
+	
+	cout << "Hello";
+	cout << "Hello";
+	cout << "Hello";
+
+}
