d760fce46b259a7617de72d0a77d5c22f58f135c
WASlavagit config --global user.email WASlava@gmail,comgit config --global user.name WASlava <waslava@gmail.com>
2023-01-18 23:37:59 +0100

    Change func1() and add for used it.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int func1(int x)
 {
+	int n = x;
+	for (size_t i = 1; i < n-1; i++)
+	{
+		x *= (n - i);
+	}
 	return x;
 }
 int func2(int x)
@@ -22,7 +27,7 @@ struct MyStruct
 };
 int main() 
 {
-	
+	int x = 0;
 
 	cout << "Hello";
 	cout << "Hello"; 
@@ -32,7 +37,16 @@ int main()
 	cout << "World";
 
 	cout << "HelloWorld";
-
+	cout << "\nEnter x (from 1 to 99): x=";
+	cin >> x;
+	if (x>0 && x<100)
+	{
+		printf_s("%d!=%d\n", x, func1(x));
+	}
+	else
+	{
+		cout << "You are wrong." << endl;
+	}
 
 
 
